d452ba89576ba45b4ad77e688f4cc0c073a00511
Sergei <pazzaru@gmail.com>
2024-02-02 20:03:19 +0900

    popup

--- popup.js
+++ popup.js
@@ -1,2 +1,50 @@
-const navRadio = document.querySelectorAll('nav radio');
+const navRadio = document.querySelectorAll('nav label');
 const menu = document.querySelectorAll('.menu');
+
+navRadio.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        menu.forEach(item => item.classList.remove('active'));
+        menu[index].classList.add('active');
+    })
+
+})
+
+function checkforUpdates() {
+    let version = null;
+    fetch('./VERSION')
+        .then(response => response.text())
+        .then(data => {
+            version = data;
+            document.querySelector('#version').textContent = version;
+            const lastChecked = localStorage.getItem('lastChecked');
+            let updateAvailableSaved = localStorage.getItem('newVersionAvailable');
+
+            function showUpdatePopup(ver) {
+                const newVer = document.querySelector('.new-version');
+                newVer.classList.add('active');
+                newVer.querySelector('span').innerText = ver;
+            }
+            
+            if (!updateAvailableSaved) {
+                if (lastChecked === null || lastChecked - new Date() > 3600000) {
+                    fetch('https://raw.githubusercontent.com/qlimq/ek5enchancement/main/VERSION')
+                    .then(response => response.text())
+                    .then(data => {
+                        if (data > version) {
+                            const newVer = data.match(/[0-9]{1,2}[.]\d{1,2}/)[0];
+                            showUpdatePopup(newVer);
+                            localStorage.setItem('newVersionAvailable', newVer);
+                        }
+                    })
+                }
+            } else {
+                if (updateAvailableSaved > version) {
+                    showUpdatePopup(updateAvailableSaved)
+                }
+            }
+            localStorage.setItem('lastChecked', new Date());
+
+        });
+
+}
+checkforUpdates()
